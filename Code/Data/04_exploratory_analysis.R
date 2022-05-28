4debe2bd9b6f61c23f87b604192ed9afb08707c5
StackOverthink <blasch57@gmail.com>
2022-05-28 20:39:15 +0200

    Exploratory Analysis

--- Code/Data/04_exploratory_analysis.R
+++
@@ -1,5 +0,0 @@
-# source AUX
-source("./Misc/Auxilliary.R")
-
-# load data
-auctions <- readRDS("./../Data/Bid Tab RDS/Bid_Tabs.RDS")
--- Code/Misc/Auxilliary.R
+++ Code/Misc/Auxilliary.R
@@ -20,4 +20,47 @@ invis.rapply <- function(object, f, classes = "ANY", deflt = NULL,
                          invisible(rapply(object, f, classes = "ANY", deflt = NULL,
                          how = c("unlist", "replace", "list"), ...))}
 
+## Handling Auction Data ##
+
+
+# function for pulling variables across all desired years and auctions
+pull_varT <- \(auctions, var, years = as.character(2015:2019)){
+  
+  # pull
+  do.call(c, 
+          
+    # over years      
+    lapply(auctions[years], \(x){
+
+      # over auctions
+      sapply(x, \(y) y[["Text"]][var])
+    
+  }))
+  
+}
+
+# function for altering a specific variable across all desired years and auctions
+alt_varT <- \(auctions, var, years = as.character(2015:2019), Fun){
+  
+  # over years
+  for(i in seq_along(years)){
+    
+    # over contract ID
+    for(j in seq_along(auctions[[i]])){
+
+      # alter
+      auctions[[i]][[j]][["Text"]][var] <- Fun(auctions[[i]][[j]][["Text"]][var])
+      
+      
+    }
+    
+  }
+
+  # return
+  return(auctions)
+  
+}
+
+
+
 
