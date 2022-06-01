b63b7d4c60c2694240b8c29fa1249548af83bd63
StackOverthink <blasch57@gmail.com>
2022-06-01 11:30:20 +0200

    Data Transformations, Descriptive Statistics

--- Code/Misc/Auxilliary.R
+++ Code/Misc/Auxilliary.R
@@ -88,6 +88,34 @@ d_transform <- \(auc){
   return(res)
 }
 
+## Plotting ##
+
+# Normal plots
+Dens_norm_plot <- \(data = dat_bids, y, bg_alt = FALSE){
+  
+  # init. plot
+  ggplot(data, aes(.data[[y]])) +
+    
+    # hist
+    geom_histogram(aes(y = ..density..), fill = "cornflowerblue", col = "deepskyblue4") +
+    
+    # fit normal
+    stat_function(fun = dnorm, 
+                  args = list(mean = mean(data[[y]], na.rm = TRUE), 
+                              sd = sd(data[[y]], na.rm = TRUE)),
+                  col = "darkblue", lwd = 1, lty = "dashed") +
+    theme_bw() +
+    
+    # add colored grid optionally
+    {if(bg_alt) theme(panel.background = element_rect(fill = "#90EE90",
+                                                      size = 2, linetype = "solid"),
+                      panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                                      colour = "white"), 
+                      panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                                      colour = "white"))}
+  
+}
+
 
 
 
