1ca778f45ba8a51e7ccc93c3a02bc0e41ed722ee
StackOverthink <blasch57@gmail.com>
2022-06-16 02:48:37 +0200

    csv for python import

---
+++ Data/Bid Tab RDS/Colab Transfer/csv_convert.R
@@ -0,0 +1,22 @@
+# files 
+files <- list.files("./../Data/Bid Tab RDS/Colab Transfer")
+files <- files[stringr::str_detect(files, ".RDS")]
+file_names <- stringr::str_remove(files, ".RDS")
+  
+# loop over dfs and create csv
+Map(\(x, n){
+  
+  # import
+  imp <- readRDS(paste0("./../Data/Bid Tab RDS/Colab Transfer/", x))
+
+  # if data.frame then
+  if(class(imp)[3] == "data.frame"){
+
+    # write csv
+    write.csv(imp, 
+              paste0("./../Data/Bid Tab RDS/Colab Transfer/csv/", n, ".csv"))
+    
+  }
+  
+}, files, file_names)
+
