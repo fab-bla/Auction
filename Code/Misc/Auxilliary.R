659929587cd5f901c5d620af36237669acb68c78
StackOverthink <blasch57@gmail.com>
2022-06-19 17:18:19 +0200

    New Aux Fun MLE estimated density histogram overlay

--- Code/Misc/Auxilliary.R
+++ Code/Misc/Auxilliary.R
@@ -154,7 +154,7 @@ d_transform_2 <- \(auc){
 
 ## Plotting ##
 
-# Normal plots
+# normal with hist
 Dens_norm_plot <- \(data = dat_bids, y, bg_alt = FALSE){
   
   # init. plot
@@ -179,6 +179,37 @@ Dens_norm_plot <- \(data = dat_bids, y, bg_alt = FALSE){
                                                       colour = "white"))}
   
 }
+
+# generalization using MASS
+
+Dens_hist_plot <- \(data = dat_bids, y, dist = "gamma", distFUN = dgamma, bg_alt = FALSE, lower_b = NULL, bins = 30){
+  
+  # Estimate paramerters via MLE
+  dis_MLE <- MASS::fitdistr(data[[y]], densfun = dist, lower = lower_b)
+  
+  # init. plot
+  ggplot(data, aes(.data[[y]])) +
+    
+    # hist
+    geom_histogram(aes(y = ..density..), fill = "cornflowerblue", col = "deepskyblue4",
+                   bins = bins) +
+    
+    # fit normal
+    stat_function(fun = distFUN, 
+                  args = as.list(dis_MLE$estimate),
+                  col = "darkblue", lwd = 1, lty = "dashed") +
+    theme_bw() +
+    
+    # add colored grid optionally
+    {if(bg_alt) theme(panel.background = element_rect(fill = "#90EE90",
+                                                      size = 2, linetype = "solid"),
+                      panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                                      colour = "white"), 
+                      panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                                      colour = "white"))}
+  
+}
+
 
 
 
