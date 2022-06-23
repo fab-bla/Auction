84c0f9e3700b2277eaab40775c96fd6c4a88544d
StackOverthink <blasch57@gmail.com>
2022-06-23 16:32:43 +0200

    Corr. feature engineering & more papers

--- Data/Bid Tab RDS/Colab Transfer/csv_convert.R
+++ Data/Bid Tab RDS/Colab Transfer/csv_convert.R
@@ -1,29 +1,44 @@
 # files 
 setwd("C:/Users/blasc/OneDrive/Documents/GitHub/Auction/Code")
-files <- list.files("./../Data/Bid Tab RDS/Colab Transfer")
-files <- files[stringr::str_detect(files, ".RDS")]
-file_names <- stringr::str_remove(files, ".RDS")
-  
-# loop over dfs and create csv
+dat_aucs <- readRDS("./../Data/Bid Tab RDS/Aucs_df_feateng.RDS")
+split_ind <- readRDS("./../Data/Bid Tab RDS/Colab Transfer/split_indices.RDS")
+
+# write csvs
+write.csv(dat_aucs, "./../Data/Bid Tab RDS/Colab Transfer/csv/dat_aucs_feateng.csv",
+          row.names = FALSE)
+
+# indices
 Map(\(x, n){
   
-  # import
-  imp <- readRDS(paste0("./../Data/Bid Tab RDS/Colab Transfer/", x))
-  
-  # if data.frame then
-  if(class(imp) == "data.frame" | class(imp)[3] == "data.frame"){
+  # write
+  write.csv(x, paste0("./../Data/Bid Tab RDS/Colab Transfer/csv/", n, ".csv"),
+            row.names = FALSE)
 
-    # write csv
-    write.csv(imp, 
-              paste0("./../Data/Bid Tab RDS/Colab Transfer/csv/", n, ".csv"),
-              row.names = FALSE)
-    
-    # excel
-    openxlsx::write.xlsx(imp, 
-                         paste0("./../Data/Bid Tab RDS/Colab Transfer/csv/", n, 
-                                ".xlsx"))
-  }
   
-}, files, file_names)
+}, split_ind, names(split_ind))
+
+
+
+# loop over dfs and create csv
+# Map(\(x, n){
+#   
+#   # import
+#   imp <- readRDS(paste0("./../Data/Bid Tab RDS/Colab Transfer/", x))
+#   
+#   # if data.frame then
+#   if(class(imp) == "data.frame" | class(imp)[3] == "data.frame"){
+# 
+#     # write csv
+#     write.csv(imp, 
+#               paste0("./../Data/Bid Tab RDS/Colab Transfer/csv/", n, ".csv"),
+#               row.names = FALSE)
+#     
+#     # excel
+#     openxlsx::write.xlsx(imp, 
+#                          paste0("./../Data/Bid Tab RDS/Colab Transfer/csv/", n, 
+#                                 ".xlsx"))
+#   }
+#   
+# }, files, file_names)
 
 
