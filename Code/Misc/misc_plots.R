fd1c5f6dffb8f9a62c4f51840a01b87798580c14
StackOverthink <blasch57@gmail.com>
2022-08-27 13:32:41 +0200

    Init. prediction section

--- Code/Misc/misc_plots.R
+++ Code/Misc/misc_plots.R
@@ -1,8 +1,8 @@
 # packages
 library(ggplot2)
-
+setwd("./GitHub/Auction")
 # linear and quadratic loss
-x <- seq(-1.5, 1.5, 0.001)
+x <- seq(-2, 2, 0.001)
 ys <- x^2
 ya <- abs(x)
 df <- data.frame(x, ys, ya)
@@ -11,19 +11,20 @@ df <- data.frame(x, ys, ya)
 showtext::showtext_auto()
 sysfonts::font_add("lmmodern", 
                    "C:/Users/blasc/OneDrive/Documents/R/win-library/4.1/sysfonts/fonts/lmroman10-regular.ttf")
+
 # write into thesis folder
-# pdf("./../../Thesis/figures/barplots.pdf", width = 10, height = 6)
+pdf("./Thesis/figures/Lossfun.pdf", width = 10, height = 6)
 
 # align
-#par(mfrow = c(2, 2), mar = c(4, 2.5, 2, 1), family = "lmmodern")
+par(mar = c(4, 4, 2, 1), family = "lmmodern")
 
-# plot 
-ggplot(df, aes(x)) +
-  geom_line(aes(y = ys), color = "forestgreen") +
-  geom_line(aes(y = ya), color = "cornflowerblue") +
-  theme_bw() +
-  xlab("") +
-  ylab("Loss") 
-  
+plot(x, ys, type = "n", xlab = "Error", ylab = "Loss")
+grid(lwd = 1.5)
+lines(x,ys, col = "forestgreen", lwd = 2)
+lines(x,ya, col = "cornflowerblue", lwd = 2)
+legend("top", legend = c("Quadratic Loss", "Linear Loss"), 
+       col = c("forestgreen", "cornflowerblue"), lty = 1, lwd = 2)
+dev.off()
+
   
   
